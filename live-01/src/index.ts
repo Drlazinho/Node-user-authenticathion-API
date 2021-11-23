81d87b86ca892a6dd97c55322424c556a199a14a
Lázaro Pimentel <lazbonfim@hotmail.com>
2021-11-23 11:54:55 -0300

    Adicionando o typescript e incrementando a forma de desenvolver

--- live-01/src/index.ts
+++ live-01/src/index.ts
@@ -1,1 +1,13 @@
-console.log('Facil');
+import express, {Request, Response, NextFunction}from 'express';
+// Express é um gerenciador de rotas via HTTP.
+
+const app = express();
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({foo: 'sucesso, atualizando a todo momento'});
+});
+
+
+app.listen(3000, () => {
+  console.log('executando')
+})
