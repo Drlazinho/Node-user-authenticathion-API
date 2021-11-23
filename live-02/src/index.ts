427a719294b57132d48f9db5b2c05163568e844d
Lázaro Pimentel <lazbonfim@hotmail.com>
2021-11-23 16:20:45 -0300

    Live-02

---
+++ live-02/src/index.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import statusRoutes from './routes/status.route';
+// Express é um gerenciador de rotas de API REST.
+import usersRouter from './routes/users.route';
+
+const app = express();
+
+//Configuração de aplicação
+app.use(express.json()) //adicionando um  middle que é responsavel por interpretar o content/type. Nesse caso o JSON.
+app.use(express.urlencoded({extended: true}));
+
+//Configuração de rotas
+app.use(usersRouter)
+app.use(statusRoutes)
+
+//inicialização do servidor
+app.listen(3000, () => {
+  console.log('executando na porta 3000')
+})
---
+++ live-02/src/routes/users.route.ts
@@ -0,0 +1,44 @@
+import {Router, Response, Request, NextFunction} from 'express';
+import {StatusCodes} from 'http-status-codes'
+
+const usersRouter = Router();
+
+usersRouter.get('/users', (req: Request, res: Response, next: NextFunction) => {
+  const users = [{userName: 'Lázaro'}];
+  res.status(StatusCodes.OK).send(users);
+})
+
+usersRouter.get('/users/:uuid', (req: Request<{ uuid: string}>, res: Response, next: NextFunction) => {
+  const uuid = req.params.uuid;
+  res.status(StatusCodes.OK).send({uuid})
+})
+
+usersRouter.post('/users', (req: Request, res: Response, next: NextFunction) => {
+  const newUser = req.body;
+
+  console.log(req.body);
+  res.status(StatusCodes.CREATED).send(newUser)
+})
+
+usersRouter.put('/users/:uuid',(req: Request<{ uuid: string}>, res: Response, next: NextFunction) => {
+  const uuid = req.params.uuid;
+  const modifiedUser = req.body;
+
+  modifiedUser.uuid = uuid
+
+  res.status(StatusCodes.OK).send({modifiedUser});
+});
+
+usersRouter.delete('/users/:uuid',(req: Request<{ uuid: string}>, res: Response, next: NextFunction) => {
+  res.sendStatus(StatusCodes.OK);
+
+})
+
+export default usersRouter
+
+
+//get /users
+//get /users/:uuid
+//post /users
+// put /users/:uuid
+//delet /users/:uuid
